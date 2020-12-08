e54d9d725d48a2cad44da2a27469944ca6d1b6df
DanielGara <yo@danielgara.com>
2020-12-08 14:41:54 -0500

    chapter03

---
+++ chapter03/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id='app'>
+    <current-time class='col-4' />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Options } from 'vue-class-component';
+import CurrentTime from './components/CurrentTime.vue';
+
+@Options({
+  components: {
+    CurrentTime
+  },
+})
+export default class App extends Vue {
+  
+}
+</script>
+
+<style lang="stylus">
+#app
+  font-family Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+</style>
---
+++ chapter03/src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import store from './store';
+import router from './router';
+import './assets/css/style.css';
+
+createApp(App).use(router).use(store).mount('#app');
