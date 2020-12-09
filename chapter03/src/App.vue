6c6c329c2a6a578fb971c1f58040c941888a8ca6
DanielGara <yo@danielgara.com>
2020-12-09 07:24:46 -0500

    chapter 04 - slots

--- chapter03/src/App.vue
+++ chapter03/src/App.vue
@@ -151,7 +151,28 @@ export default class App extends Vue {
 </script>
 
 <style scoped>
-  .taskList li{
-    text-align: left;
+.taskList li {
+  list-style: none;
+  text-align: left;
+  padding: 5px 10px;
+  border-bottom: 1px solid rgba(0,0,0,0.15);
+}
+.taskList li:last-child {
+  border-bottom: 0px;
+}
+.taskList li:nth-child(even){
+  background-color: rgba(0,0,0,0.05);
+}
+@keyframes colorChange {
+  from{
+    background-color: inherit;
   }
+  to{
+    background-color: rgba(0, 160, 24, 0.577);
+  }
+}
+.taskList li.taskDone{
+  animation: colorChange 1s ease;
+  background-color: rgba(0, 160, 24, 0.577);
+}
 </style>
---
+++ chapter04/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <MaterialCardBox>
+      <template v-slot:header>
+        <strong>Card Title</strong><br>
+        <span>Card Sub-Title</span>
+      </template>
+      <template v-slot:media>
+        <img src="https://via.placeholder.com/350x150">
+      </template>
+      <p>Main Section</p>
+      <template v-slot:action>
+        <button>Action Button</button>
+        <button>Action Button</button>
+      </template>
+    </MaterialCardBox>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Options } from 'vue-class-component'
+import MaterialCardBox from '@/components/MaterialCardBox.vue';
+
+@Options({
+  components: {
+    MaterialCardBox
+  }
+})
+export default class App extends Vue {
+}
+</script>
+
+<style lang="stylus">
+#app
+  font-family Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+</style>
