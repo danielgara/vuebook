faee06dc06619a350f630695ae5467ad7e5fe4f1
DanielGara <yo@danielgara.com>
2020-12-10 08:26:21 -0500

    middlewares

--- chapter06/src/router/index.ts
+++ chapter06/src/router/index.ts
@@ -2,28 +2,50 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import Contact from '../views/contact.vue';
 import About from '../views/About.vue';
+import Login from '../views/Login.vue';
+import AuthenticationMiddleware from './middleware/authentication';
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: Home,
+    meta: {
+      authenticated: false,
+    },
   },
   {
     path: '/contact',
     name: 'contact',
     component: Contact,
+    meta: {
+      authenticated: true,
+    },
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+    meta: {
+      authenticated: false,
+    },
   },
   {
     path: '/about',
     name: 'about',
     alias: '/about-us',
     component: About,
+    meta: {
+      authenticated: false,
+    },
   },
   {
     path: '/home',
     name: 'home2',
     redirect: '/',
+    meta: {
+      authenticated: false,
+    },
   },
 ];
 
@@ -31,5 +53,7 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes,
 });
+
+router.beforeEach(AuthenticationMiddleware);
 
 export default router;
---
+++ chapter06/src/views/Login.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container">
+    <h3>
+        User Login
+    </h3>
+
+    <input
+        label="Username"
+        placeholder="Username or e-mail"
+        v-model="username"
+        />
+    
+    <input
+        label="Password"
+        type="password"
+        placeholder="Your password"
+        v-model="password"
+        />
+
+        <button @click="userSignIn"
+        >
+        Sign-in
+        </button>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Login',
+    data: () => ({
+      username: '',
+      password: '',
+      error: false,
+    }),
+    methods: {
+      userSignIn() {
+        if (this.username && this.password) {
+          window.sessionStorage.setItem('auth',
+            window.btoa(JSON.stringify({
+                username: this.username
+              })
+            )
+          );
+          this.error = false;
+          this.$router.replace('/');
+        }
+        this.error = true;
+        return false;
+      },
+    }
+  };
+</script>
+<style scoped>
+  .container {
+    height: 100vh;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-content: center;
+  }
+  .vs-input {
+    margin: 5px;
+  }
+</style>
